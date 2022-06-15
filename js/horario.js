d409137eb9e45aa1ddcff2f48f3b653630a50758
= <=>
2022-06-15 16:10:10 +0100

    horario injetado pelo JavaScript

---
+++ js/horario.js
@@ -0,0 +1,40 @@
+class Horario {
+  constructor(hora, numDiasDeTraballho) {
+    this.hora = hora;
+    this.horaTermino = this.hora + 8;
+    this.numDiasDeTraballho = numDiasDeTraballho;
+    this.lista = Array.from(
+      { length: numDiasDeTraballho },
+      this.registar.bind(this)
+    );
+  }
+  registar(_, numeroDaSemana) {
+    const data = new Date(0, 0, numeroDaSemana);
+    const diaDaSemana = data.toLocaleString("pt-BR", { weekday: "long" });
+    return {
+      nome: diaDaSemana.toUpperCase(),
+      horario: `${this.hora}H - ${this.horaTermino}H`,
+    };
+  }
+  setHorarioDoDia(nome, horario) {
+    this.lista.find((semana) => semana.nome === nome.toUpperCase()).horario =
+      horario.toUpperCase();
+  }
+  exibir() {
+    const $lista = $("#horario-lista");
+    let html = "";
+
+    this.lista.forEach((dia) => {
+      html += `
+        <li>
+            <span>${dia.nome} </span><span>${dia.horario}</span>
+        </li>
+        `;
+    });
+    $lista.html(html);
+  }
+}
+
+const horario = new Horario(9, 7);
+horario.setHorarioDoDia("Domingo", "Fechado");
+horario.exibir();
